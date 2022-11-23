18eddf01c02c39d6e9f38bab9660e9b9c3fbd459
Sukant <93847372+sukantsondhi@users.noreply.github.com>
2022-11-23 17:53:58 +0000

    Update Snail.py

--- Snail.py
+++ Snail.py
@@ -1,5 +1,4 @@
-#A snail climbs on a wall, it starts at the bottom and climbs up nn meters a day. Regretfully, every night it slides mm meters down.
-#Given the height of the wall HH, write a program to calculate how many days required for the snail to reach the top of the wall.
+
 import sys
 
 for line in sys.stdin:
